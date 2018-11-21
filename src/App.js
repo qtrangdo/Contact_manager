58b1e69b8c95d07499b0f3387ad394eb0a6486b7
td94 <quynhtrangdo94@gmail.com>
2018-11-21 11:43:56 -0500

    Adding Context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import Contacts from './components/Contacts'
-import Header from './components/Header'
+import Contacts from './components/Contacts';
+import Header from './components/Header';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from './context';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header branding="Contact Manager" />
-        <div className='container'>
-          <Contacts />
-        </div> 
-      </div>
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className='container'>
+            <Contacts />
+          </div> 
+        </div>
+      </Provider>
+      
     );
   }
 }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,32 +1,11 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
+import { Consumer } from '../context';
 
 class Contacts extends Component {
-    state = {
-        contacts: [
-            {
-                id: 1,
-                name: 'John Doe',
-                email: 'jdoe@gmail.com',
-                phone: '555-555-5555'
-            },
-            {
-                id: 2,
-                name: 'Karen Williams',
-                email: 'karen@gmail.com',
-                phone: '222-222-2222'
-            },
-            {
-                id: 3,
-                name: 'Henry Johnson',
-                email: 'henry@gmail.com',
-                phone: '111-111-1111'
-            }
-        ]
-    }
 
     deleteContact = (id) => {
-        const { contacts } =this.state;
+        const { contacts } = this.state;
         const newContacts = contacts.filter(contact => {
             return contact.id !== id;
         })
@@ -36,19 +15,24 @@ class Contacts extends Component {
     }
 
     render() {
-        const { contacts } = this.state;
         return (
-            <div>
-                { contacts.map(contact => {
-                    return <Contact
-                        key = { contact.id }
-                        contact = { contact }
-                        deleteClickHandle = { this.deleteContact.bind(this, contact.id) }
-                    />
-                })}
-            </div>
+            <Consumer>
+                {value => {
+                    const { contacts } = value;
+                    return (
+                        <div>
+                            {contacts.map(contact => {
+                                return <Contact
+                                    key={contact.id}
+                                    contact={contact}
+                                    deleteClickHandle={this.deleteContact.bind(this, contact.id)}
+                                />
+                            })}
+                        </div>
+                    )
+                }}
+            </Consumer>
         )
-       
     }
 }
 
